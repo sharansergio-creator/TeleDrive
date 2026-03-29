62b8e10ba043b3774e81f89354678b1700d10f67
Sharan S <sharansergio@gmail.com>
2026-03-29 11:41:54 +0530

    Clean repo: removed duplicate app folder

--- app/src/main/java/com/teledrive/app/analysis/DrivingAnalyzer.kt
+++
@@ -1,8 +0,0 @@
-package com.teledrive.app.analysis
-
-import com.teledrive.app.core.FeatureVector
-import com.teledrive.app.core.DrivingEvent
-
-interface DrivingAnalyzer {
-    fun analyze(features: FeatureVector, speed: Float): DrivingEvent
-}
--- app/src/main/java/com/teledrive/app/core/LiveDataBus.kt
+++
@@ -1,5 +0,0 @@
-package com.teledrive.app.core
-
-object LiveDataBus {
-    var listener: ((String, Float, Float) -> Unit)? = null
-}
